bfed07a2c4416ad5d11cc9d65137e192c420af3d
Max Plumer <maxplumer12@gmail.com>
2023-10-02 20:21:05 -0400

    testimonial card styling

--- src/components/sections/homePage/TestimonialCard.js
+++ src/components/sections/homePage/TestimonialCard.js
@@ -3,7 +3,13 @@ import Star from '../../../assets/star.png';
 export default function TestimonialCard(props) {
     return (
         <article className="testimonial-card">
+            <div className="stars">
             <img src={Star} alt="star Icon"></img>
+            <img src={Star} alt="star Icon"></img>
+            <img src={Star} alt="star Icon"></img>
+            <img src={Star} alt="star Icon"></img>
+            <img src={Star} alt="star Icon"></img>
+            </div>
             <h3>{props.name}</h3>
             <p>{props.description}</p>
         </article>
